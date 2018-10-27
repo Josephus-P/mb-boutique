5692d61592e430159c33ff96369fdcea312ae632
Josue Peralta <jperal77@gmail.com>
2018-10-27 02:36:43 -0500

    Editted carousel swipeScrollTolerance

--- src/components/mbcarousel.js
+++ src/components/mbcarousel.js
@@ -16,7 +16,6 @@ export default class MBCarousel extends Component {
         emulateTouch={true}
         infiniteLoop={true}
         interval={8000}
-        swipeScrollTolerance={0}
       >
         <Container>
           <Row>
