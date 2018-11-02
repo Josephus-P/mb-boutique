4ed05c3a8f7b97564dc3464b48004e38e237ea02
Josue Peralta <jperal77@gmail.com>
2018-11-02 02:18:46 -0500

    added site icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,8 +21,8 @@ module.exports = {
         start_url: '/',
         background_color: '#663399',
         theme_color: '#663399',
-        display: 'minimal-ui'
-        // This path is relative to the root of the site.
+        display: 'minimal-ui',
+        icon: 'src/images/icon.png'
       }
     },
     'gatsby-plugin-offline',
