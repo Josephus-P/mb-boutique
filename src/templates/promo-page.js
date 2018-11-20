77480f4d7dd8f15b8d019a1ef6054386e5b7e3ca
Josue Peralta <jperal77@gmail.com>
2018-11-20 14:37:02 -0600

    Added styling to promo page

--- src/templates/promo-page.js
+++ src/templates/promo-page.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import { graphql } from 'gatsby';
+import { Container, Row, Col } from 'reactstrap';
+import Layout from '../components/layout';
+import '../scss/promo.scss';
+import 'typeface-playfair-display';
+import 'typeface-poppins';
 
 export default function Template({
   data // this prop will be injected by the GraphQL query below.
@@ -7,16 +12,20 @@ export default function Template({
   const { markdownRemark } = data; // data.markdownRemark holds our post data
   const { frontmatter, html } = markdownRemark;
   return (
-    <div className="promo-page">
-      <div className="promo-page-container">
-        <h1>{frontmatter.title}</h1>
-        <p>{frontmatter.description}</p>
-        <div
-          className="blog-post-content"
-          dangerouslySetInnerHTML={{ __html: html }}
-        />
-      </div>
-    </div>
+    <Layout>
+      <Container className="promo-page-container">
+        <Row>
+          <Col xs="12">
+            <h1>{frontmatter.title}</h1>
+            <p>{frontmatter.description}</p>
+            <div
+              className="blog-post-content"
+              dangerouslySetInnerHTML={{ __html: html }}
+            />
+          </Col>
+        </Row>
+      </Container>
+    </Layout>
   );
 }
 
