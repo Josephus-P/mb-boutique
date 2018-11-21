332a15d31498672fe673b59b3fd100d0c01555a1
Josue Peralta <jperal77@gmail.com>
2018-11-21 14:40:59 -0600

    added a button and meta tags

--- src/templates/promo-page.js
+++ src/templates/promo-page.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { graphql } from 'gatsby';
+import { Link, graphql } from 'gatsby';
 import { Container, Row, Col } from 'reactstrap';
 import Layout from '../components/layout';
+import Helmet from 'react-helmet';
 import '../scss/promo.scss';
 import 'typeface-playfair-display';
 import 'typeface-poppins';
@@ -9,6 +10,19 @@ import 'typeface-poppins';
 export const PromoPageTemplate = ({ title, description, content, image }) => {
   return (
     <Layout>
+      <Helmet title="Microblading Services, Oak Lawn | Microblading Boutique">
+        <html lang="en" />
+        <meta
+          name="description"
+          content="Beautiful eyebrows done by an expert. Book your appointment online now for a professional microblading session in the Chicagoland area."
+        />
+        <meta property="og:title" content={`${title}`} />
+        <meta property="og:description" content={`${description}`} />
+        <meta
+          property="og:image"
+          content={`https://browsbymonica.com/static${image}`}
+        />
+      </Helmet>
       <Container className="promo-page-container">
         <Row>
           <Col xs="12">
@@ -16,6 +30,12 @@ export const PromoPageTemplate = ({ title, description, content, image }) => {
             <img className="img-fluid" src={image} alt="promotion" />
             <p>{description}</p>
             <div dangerouslySetInnerHTML={{ __html: content }} />
+
+            <Link to="/appointment">
+              <button aria-label="schedule-appointment">
+                Book your Appointment!
+              </button>
+            </Link>
           </Col>
         </Row>
       </Container>
